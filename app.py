8fff8a55039967c1687b266e91c2962b5cc1b40b
Billynguy <67550894+Billynguy@users.noreply.github.com>
2022-10-26 00:24:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
 
 app = Flask(__name__, static_url_path='', static_folder='ui/build/')
+
 
 @app.route('/')
 def index():
     return send_from_directory('ui/build/', 'index.html')
+
+@app.route('/joinProject/<projectid>')
+def joinProject(projectid):
+    return projectid
+
+
+@app.route('/leaveProject/<projectid>')
+def leaveProject(projectid):
+    return projectid
+
+
+@app.route('/checkIn/<projectid>/<qty>')
+def checkIn_hardware(projectid, qty):
+    jsonResp = {'name': projectid, 'qty': qty}
+    return jsonResp
+
+
+@app.route('/checkOut/<projectid>/<qty>')
+def checkOut_hardware(projectid, qty):
+    jsonResp = {'name': projectid, 'qty': qty}
+    return jsonResp
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
