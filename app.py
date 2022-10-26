cc2f21574bf6498233d99101ed09501ad1fe8845
Billy <billynguy20@gmail.com>
2022-10-26 00:46:12 -0500

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, after_this_request
 
 app = Flask(__name__, static_url_path='', static_folder='ui/build/')
 
@@ -10,22 +10,38 @@ def index():
 
 @app.route('/joinProject/<projectid>')
 def joinProject(projectid):
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     return projectid
 
 
 @app.route('/leaveProject/<projectid>')
 def leaveProject(projectid):
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     return projectid
 
 
 @app.route('/checkIn/<projectid>/<qty>')
 def checkIn_hardware(projectid, qty):
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     jsonResp = {'name': projectid, 'qty': qty}
     return jsonResp
 
 
 @app.route('/checkOut/<projectid>/<qty>')
 def checkOut_hardware(projectid, qty):
+    @after_this_request
+    def add_header(response):
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     jsonResp = {'name': projectid, 'qty': qty}
     return jsonResp
 
